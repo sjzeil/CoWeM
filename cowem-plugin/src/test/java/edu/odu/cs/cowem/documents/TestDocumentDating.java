477e35edaa79ed06f8fe0a89a1cfba609283ba93
Steven Zeil <zeil@cs.odu.edu>
2017-05-26 16:34:03 -0400

    Working on pulling dates out of git repository

--- cowem-plugin/src/test/java/edu/odu/cs/cowem/documents/TestDocumentDating.java
+++ cowem-plugin/src/test/java/edu/odu/cs/cowem/documents/TestDocumentDating.java
@@ -1,25 +1,33 @@
 package edu.odu.cs.cowem.documents;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
-import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.diff.RawTextComparator;
+import org.eclipse.jgit.errors.CorruptObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.util.io.DisabledOutputStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestDocumentDating {
 
@@ -51,34 +59,74 @@ public class TestDocumentDating {
                 .setGitDir(repoDir)
                 .build();
         assertTrue (repoDir.exists());
-        Ref master = repo.findRef("master");
         
         Git git = new Git(repo);
-        String existingFile = "TestMarkdownDocument.java";
+        String existingFile = "TestDocumentDating.java";
+        long lastUpdatedOn = 0;
 
-        ArrayList<RevCommit> commits = new ArrayList<RevCommit>();
         Iterable<RevCommit> log = git.log().call();
         for (RevCommit commit : log) {
-            //System.err.println(commit.getShortMessage() + " " + commit.getCommitTime());
+            int commitTime = commit.getCommitTime();
+            if (commitTime > lastUpdatedOn) {
+                System.err.println(commit.getShortMessage() + " " + commitTime);
+                if (commitContains(commit, existingFile, repo)) {
+                    lastUpdatedOn = commitTime;
+                }
+            }
+        }
+        assertTrue (lastUpdatedOn > 0);
+        Calendar dateChanged = new GregorianCalendar();
+        dateChanged.setTimeInMillis(1000L * lastUpdatedOn);
+        assertEquals(2017, dateChanged.get(Calendar.YEAR));
+        assertEquals (4, dateChanged.get(Calendar.MONTH));
+        git.close();
+    }
+
+
+    private boolean commitContains(RevCommit commit, 
+            String existingFile, 
+            Repository repo) 
+                    throws MissingObjectException, 
+                    IncorrectObjectTypeException, CorruptObjectException, 
+                    IOException
+    {
+        if (commit.getParentCount() == 0) {
+            // No parent. (First commit?)
             RevTree tree = commit.getTree(); 
             TreeWalk treeWalk = new TreeWalk(repo);
             treeWalk.addTree(tree);
             treeWalk.setRecursive(true);
-            CanonicalTreeParser canonicalTreeParser = treeWalk.getTree(0, CanonicalTreeParser.class);
-            while (!canonicalTreeParser.eof()) {
-                // if the filename matches, we have a match, so set the byte array to return
-                String committedFile = canonicalTreeParser.getEntryPathString();  
-                if (committedFile.endsWith(existingFile)) {
-                    System.err.println("Found " + committedFile + " at " + commit.getCommitTime());
+            while (treeWalk.next()) {
+                String path = treeWalk.getPathString();
+                System.out.println("no parent: " + path);
+                if (path.endsWith(existingFile)) {
+                    treeWalk.close();
+                    return true;
                 }
-                canonicalTreeParser.next(1);
             }
-
-            commits.add(commit);
-
+            treeWalk.close();
+            return false;
+        } else {
+            RevWalk rw = new RevWalk(repo);
+            RevCommit parent = rw.parseCommit(commit.getParent(0).getId());
+            DiffFormatter df = new DiffFormatter(DisabledOutputStream.INSTANCE);
+            df.setRepository(repo);
+            df.setDiffComparator(RawTextComparator.DEFAULT);
+            df.setDetectRenames(true);
+            List<DiffEntry> diffs = df.scan(parent.getTree(), commit.getTree());
+            for (DiffEntry diff : diffs) {
+                String path = diff.getNewPath();
+                System.out.println("parent: " + path);
+                if (path.endsWith(existingFile)) {
+                    rw.close();
+                    df.close();
+                    return true;
+                }
+            }
+            rw.close();
+            df.close();
+            return false;
         }
-        assertTrue(commits.size() > 0);
-        
     }
 
 }
