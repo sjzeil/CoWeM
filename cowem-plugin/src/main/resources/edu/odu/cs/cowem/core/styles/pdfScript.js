e55a3eb1970536e6de86d8f7623b48cbbe3094d4
zeil <szeil@odu.edu>
2021-02-05 09:09:37 -0500

    Add error handlers to pdfScript

--- cowem-plugin/src/main/resources/edu/odu/cs/cowem/core/styles/pdfScript.js
+++ cowem-plugin/src/main/resources/edu/odu/cs/cowem/core/styles/pdfScript.js
@@ -4,15 +4,19 @@ const formats = require("./website/styles/pdfFormats.js").formats;
 console.log('Node version is: ' + process.version);
 (async () => {
 	const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
-  const page = await browser.newPage();
-  var pagePath = '@scrollFile@';
-  if (pagePath.charAt(1) == ':') {
-      pagePath = '/' + pagePath.replace('\\', '/'); // Correction for Windows file URLs
-  }
-	await page.goto('file://' + pagePath);
+	const page = await browser.newPage();
+	var pagePath = '@scrollFile@';
+	if (pagePath.charAt(1) == ':') {
+		pagePath = '/' + pagePath.replace('\\', '/'); // Correction for Windows file URLs
+	}
 	try {
-	    await page.waitFor("#mathJaxHasCompleted",
-					       {visible: true, timeout: 60000});
+	    await page.goto('file://' + pagePath);
+		try {
+	        await page.waitFor("#mathJaxHasCompleted",
+					           {visible: true, timeout: 180000});
+		} catch (err) {
+			console.log ("Error eaiting for mathJax signal\n" + err);
+		}
         for (formatNum in formats) {
            formatSpec = formats[formatNum];
            console.log ("PDF format: " + formatSpec.name);
@@ -25,6 +29,9 @@ console.log('Node version is: ' + process.version);
  	} catch (err) {
  	    console.log(err);    
  	}
-	
-    await browser.close();
+	try {
+		await browser.close();
+	} catch (err) {
+		console.log("trying to close browser: " + err);
+	}
 })();
