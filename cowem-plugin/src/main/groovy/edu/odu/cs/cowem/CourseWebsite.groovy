fb746f5027abed4828ecdc6b0450c422afe41800
Steven Zeil <zeil@cs.odu.edu>
2022-01-08 10:28:08 -0500

    Remove support for PDF generation It was too fragile, slow, & bloated

--- cowem-plugin/src/main/groovy/edu/odu/cs/cowem/CourseWebsite.groovy
+++ cowem-plugin/src/main/groovy/edu/odu/cs/cowem/CourseWebsite.groovy
@@ -1,7 +1,5 @@
 package edu.odu.cs.cowem
 
-import com.moowork.gradle.node.task.NodeTask;
-
 import org.apache.tools.ant.filters.ReplaceTokens
 import org.apache.tools.ant.taskdefs.condition.Os
 
@@ -26,7 +24,6 @@ class CourseWebsite implements Plugin<Project> {
     void apply (Project project) {
         
         new org.hidetake.gradle.ssh.plugin.SshPlugin().apply(project);
-        project.getPluginManager().apply(com.moowork.gradle.node.NodePlugin.class);
         
         // Add a Course object as a property of the project
         project.extensions.create ("course", Course);
@@ -210,41 +207,6 @@ class CourseWebsite implements Plugin<Project> {
             doc.generate();
         }
 
-        project.node {  
-            version = '14.15.4' // current LTS version
-            workDir = project.file("${project.buildDir}/nodejs")
-            npmWorkDir = project.file("${project.buildDir}")
-            nodeModulesDir = project.file("${project.buildDir}")
-            download = true  // download node
-        }
-        
-        project.build.doLast {
-            project.copy {
-                from 'build/website/styles/'
-                include 'pdf-package.json'
-                into 'build/'
-                rename '.+', 'package.json'
-            }
-        }
-        project.npm_install.dependsOn('build')
-        
-        project.task ('pdfScript', type: Copy, dependsOn: 'build') {
-            from "build/website/styles/pdfScript.js"
-            into "build"
-            filter(ReplaceTokens, 
-                   tokens: [scrollFile: project.rootDir.toString().replace("\\", "/") + '/build/combined/outline/index.html'
-                            ])
-        }
-
-        project.task ('pdf', type: NodeTask, 
-                dependsOn: ['npm_install', 'singleScroll', 'pdfScript']) {
-            script = project.file('build/pdfScript.js')
-        }
-        project.pdf.doLast {
-            project.delete('build/pdfScript.js')
-        }
-        
-        
         
         
         
